78ac75db546a9e90f9afca1f96ab34e2d13347d8
Bart Read <bart.read@me.com>
2014-09-17 18:25:49 +0100

    Fixed a few syntax problems. I'm not convinced the set based approach is so good here because an error with one table causes all ALTERs and UPDATEs to fail. Possibly better to do this using a cursor where individual failures won't stop the rest of the alterations from running.

--- scripts/AlterAllNtextColumnsInDbToNvarcharMax.sql
+++ scripts/AlterAllNtextColumnsInDbToNvarcharMax.sql
@@ -1,4 +1,4 @@
-USE YOUR_DATABASE_NAME
+USE DemoDataCoData
 GO
 
 -- Migrate columns NTEXT -> NVARCHAR(MAX)
@@ -11,14 +11,14 @@ SELECT  @alterColumns = @alterColumns
 	+ QUOTENAME(c.Name)
 	+' NVARCHAR(MAX) '
 	+ CASE WHEN c.is_nullable = 1 THEN 'NOT' ELSE '' END + ' NULL;'
-	+ CHAR(13) + 'GO' + CHAR(13)
+	+ CHAR(13)
 	+ 'UPDATE '
 	+ QUOTENAME(OBJECT_SCHEMA_NAME(c.object_id)) + '.' + QUOTENAME(OBJECT_NAME(c.object_id))
 	+ ' SET '
 	+ QUOTENAME(c.Name)
 	+ ' = '
 	+ QUOTENAME(c.Name)
-	+ ';' + CHAR(13) + 'GO' + CHAR(13) + CHAR(13)
+	+ ';' + CHAR(13) + CHAR(13)
 FROM    sys.columns AS c
 INNER JOIN sys.objects AS o
 ON c.object_id = o.object_id
@@ -26,20 +26,20 @@ WHERE   o.type = 'U' AND c.system_type_id = 99; --NTEXT
 
 PRINT @alterColumns;
 
---EXECUTE sp_executesql @alterColumns;
+EXECUTE sp_executesql @alterColumns;
 GO
 
 -- Update VIEW metadata
 
 DECLARE @updateViews NVARCHAR(MAX) = '';
 SELECT @updateViews = @updateViews
-	+ 'EXECUTE sp_refreshview '
+	+ 'EXECUTE sp_refreshview '''
 	+ QUOTENAME(OBJECT_SCHEMA_NAME(o.object_id)) + '.' + QUOTENAME(OBJECT_NAME(o.object_id))
-	+ ';' + CHAR(13)
+	+ ''';' + CHAR(13)
 FROM sys.objects AS o
 WHERE o.type = 'V'
 
 PRINT @updateViews;
 
---EXECUTE sp_executesql @updateViews;
+EXECUTE sp_executesql @updateViews;
 GO
